7af10f2d99edbfabf9b70a817386031aa50a6040
Jakub Kotulski <jakub.kotulski@interia.com>
2021-10-20 09:48:24 +0200

    init

---
+++ app.js
@@ -0,0 +1,57 @@
+// const mongoose = require("mongoose");
+// const express = require("express");
+// const cors = require("cors");
+// const passport = require("passport");
+// const passportLocal = require("passport-local").Strategy;
+// const cookieParser = require("cookie-parser");
+// const bcrypt = require("bcryptjs");
+// const session = require("express-session");
+// const bodyParser = require("body-parser");
+// const app = express();
+// const Card = require("./models/card");
+
+// mongoose.connect(
+//   "mongodb+srv://praktyki:praktyki2021@development.wtktz.mongodb.net/mtg-binder",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   () => {
+//     console.log("Moongose is connected");
+//   }
+// );
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
+
+// app.use(
+//   session({
+//     secret: "secretcode",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
+
+// // app.use(cookieParser("secretcode"));
+// // app.use(passport.initialize());
+// // app.use(passport.session());
+
+// app.get("/cards", (req, res) => {
+//   Card.find({})
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// });
+
+// app.listen(4000, () => {
+//   console.log("Server has started");
+// });
---
+++ models/card.js
@@ -0,0 +1,8 @@
+// const mongoose = require("mongoose");
+// const card = new mongoose.Schema({
+//   name: String,
+//   price: Number,
+//   url: String,
+// });
+
+// module.exports = mongoose.model("card", card);
