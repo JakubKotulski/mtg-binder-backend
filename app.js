55844362ba014138b0501a6db53abc6d0b21e7e5
Jakub Kotulski <jakub.kotulski@interia.com>
2021-10-22 10:51:36 +0200

    last changes

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const bodyParser = require("body-parser");
 const app = express();
 const { getCardsAction } = require("./src/actions/get-cards");
 const { signupAction } = require("./src/actions/sign-up-user");
 const { loginAction } = require("./src/actions/login-user");
 const { getLoggedUserAction } = require("./src/actions/get-logged-user");
 const { getLoggedUserCardsAction } = require("./src/actions/get-logged-user-cards");
+const { addCard } = require("./src/actions/add-cards");
 
 mongoose.connect(
   "mongodb+srv://praktyki:praktyki2021@development.wtktz.mongodb.net/mtg-binder",
@@ -24,8 +24,8 @@ mongoose.connect(
   }
 );
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -47,10 +47,12 @@ app.use(passport.session());
 require("./src/passport-config")(passport);
 
 app.get("/cards", getCardsAction);
-app.post("/register", signupAction);
-app.post("/login", loginAction);
-app.get("/user", getLoggedUserAction);
-app.get("/logged-user-cards", getLoggedUserCardsAction);
+app.post("/cards", addCard);
+app.get("/cards/my", getLoggedUserCardsAction);
+
+app.post("/users", signupAction);
+app.post("/users/login", loginAction);
+app.get("/users/me", getLoggedUserAction);
 
 app.listen(4000, () => {
   console.log("Server has started");
